5708c321e8433344bf151b27d228b0300ab1a5df
Acherium <shinsu02@naver.com>
2025-01-12 21:13:22 +0900

    2048.2 Lyra Engine 업데이트

--- lyra/lyra-module.js
+++ lyra/lyra-module.js
@@ -3,8 +3,8 @@
 export const LYRA_NAME = "Lyra Engine";
 export const LYRA_AUTHOR = "Acherium";
 export const LYRA_CONTACT = "acherium@pm.me";
-export const LYRA_VERSION = "1115";
-export const LYRA_DATE = "25-1-5";
+export const LYRA_VERSION = "1116";
+export const LYRA_DATE = "25-1-12";
 
 export const COMMON_INTERVAL = 30;
 export const ANIMATION_INTERVAL = 30;

b7ff446fea664d9bfbf72d18ddade11ca63bf5cf
Acherium <shinsu02@naver.com>
2025-01-30 15:23:08 +0900

    2051.2 검색바 스타일 변경

--- scripts/pictor.js
+++ scripts/pictor.js
@@ -11,7 +11,7 @@ import {
     name: "Pictor",
     author: "Acherium",
     contact: "acherium@pm.me",
-    version: "2051.1",
+    version: "2051.2",
     date: "25-1-30",
     docType: "Pictor Project File",
     docVersion: 9,
